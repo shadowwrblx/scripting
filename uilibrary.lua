82f183c3edab774833971f8e011e1177aa0b8483
shadowwrblx <104780811+shadowwrblx@users.noreply.github.com>
2022-09-22 17:13:56 +0200

    Update uilibrary.lua

--- uilibrary.lua
+++ uilibrary.lua
@@ -1529,11 +1529,18 @@ function lib:Window(text, preset, closebind)
             TextBox.FocusLost:Connect(
                 function(ep)
                     if ep then
+                        if #TextBox.Text > 0 then
                             pcall(callback, TextBox.Text)
                             if disapper then
                                 TextBox.Text = ""
                             end
+                        else
+                            pcall(callback, "")
+                            if disapper then
+                                TextBox.Text = ""
+                            end
                         end
+                    end
                 end
             )
             Tab.CanvasSize = UDim2.new(0, 0, 0, TabLayout.AbsoluteContentSize.Y)
